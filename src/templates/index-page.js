9b456480aed37fe40f2d280ab48a3c9dec2ebcc9
Pascale Audet <audetpascale@gmail.com>
2021-02-18 13:53:08 -0500

    Create the index slogan theme

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,9 +1,11 @@
 import BlogSummary from "../components/BlogSummary";
+/** @jsx jsx */
+// noinspection ES6UnusedImports
+import { Box, Container, Divider, Grid, Heading, jsx, Text } from "theme-ui";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import PropTypes from "prop-types";
 import React from "react";
-import { Container, Heading, Text } from "theme-ui";
 
 const IndexPage = (props) => {
   let i = 0;
@@ -13,15 +15,49 @@ const IndexPage = (props) => {
       <Container>
         <Heading as="h1">{props.data.site.siteMetadata.title}</Heading>
         <Text>{props.data.site.siteMetadata.description}</Text>
-        <Heading as="h2">
-          {props.data.markdownRemark.frontmatter.heading}
-        </Heading>
-        <Heading as="h3">
-          {props.data.markdownRemark.frontmatter.subheading}
-        </Heading>
-        <Text
-          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-        />
+        <Grid columns={[2, "1fr 2fr"]} width={[255, null]}>
+          <Box>
+            <svg
+              viewBox="0 0 50 100"
+              sx={{ display: "inline-block", height: "100%", width: "50%" }}
+            >
+              <circle cx="50" cy="50" r="50%" sx={{ fill: "primary" }} />
+              <foreignObject
+                x="25%"
+                y="43%"
+                sx={{ height: "14%", textAlign: "right", width: "70%" }}
+              >
+                <Text as="h2" sx={{ fontSize: "5px" }}>
+                  {props.data.markdownRemark.frontmatter.heading}
+                </Text>
+              </foreignObject>
+            </svg>
+            <svg
+              viewBox="0 0 50 100"
+              style={{ display: "inline-block", height: "90%", width: "50%" }}
+            >
+              <circle cx="0" cy="50" r="50%" sx={{ fill: "secondary" }} />
+              <foreignObject
+                x="5%"
+                y="44%"
+                sx={{ height: "30%", textAlign: "left", width: "75%" }}
+              >
+                <Text as="h3" sx={{ fontSize: "5px" }}>
+                  {props.data.markdownRemark.frontmatter.subheading}
+                </Text>
+              </foreignObject>
+            </svg>
+          </Box>
+          <Box bg="muted">
+            <Text
+              dangerouslySetInnerHTML={{
+                __html: props.data.markdownRemark.html,
+              }}
+            />
+          </Box>
+        </Grid>
+        <Divider />
+        <Heading as="h2">Les derniers articles</Heading>
       </Container>
       {props.data.allMarkdownRemark.edges.map(({ node: post }) => (
         <BlogSummary key={post.id} post={post} numChild={i++} />
